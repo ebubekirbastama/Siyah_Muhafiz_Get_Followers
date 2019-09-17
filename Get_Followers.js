32397d3fcdc70b08de6d59674a4522780734554f
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-09-17 11:48:27 +0300

    Add files via upload

---
+++ Get_Followers.js
@@ -0,0 +1,13 @@
+//By & Ebubekir Bastama Instagram Get Followers list...
+//Visit Web Site:www.ebubekirbastama.com  
+
+//Get Followers Function... 
+var syhmhz = new Array();
+for (var i = 0; i < document.getElementsByClassName('FPmhX notranslate _0imsa ').length; i++) 
+{
+  syhmhz.push(document.getElementsByClassName('FPmhX notranslate _0imsa ')[i].innerText);
+}
+ for (var j = 0; j < syhmhz.length; j++) 
+{
+  document.write(syhmhz[j].toString()+"<br>");
+}
